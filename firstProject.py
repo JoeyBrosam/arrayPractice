52913cf219bd16a5385ac96094883c3346851fb3
BROSAM <jb0889658@otc.edu>
2018-10-30 08:48:41 -0500

    Added an Array

--- firstProject.py
+++ firstProject.py
@@ -1,1 +1,8 @@
 print("Hello World!")
+newListItem = 0
+myList = [1, 2, 3]
+for i in range(3):
+    newListItem = int(input("Input a number:"))
+    myList.append(newListItem)
+for i in range(len(myList)):
+    print(myList[i])
